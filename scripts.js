ce6b75552a04e88474ce9e9f04fb81a2464e1ec8
P-Welfare <pwelfare@gmail.com>
2023-12-04 11:24:55 +0700

    update new js modules

--- scripts.js
+++ scripts.js
@@ -5,18 +5,6 @@ let marker = ''
 let counter = 2
 
 
-section.forEach((div) => {
-    let index = div.getAttribute("value")
-    div.addEventListener("click", () => {
-
-
-     console.log('Hello' + `${index}`)   
-    
-    gameCount.addOX()
-    gameBoard.changeBoard(index)
-    displayController.render()
-});
-
 
 
 
@@ -34,22 +22,14 @@ section.forEach((div) => {
         _board.splice(index, 1, `${marker}`)
         console.log(_board)
     }
-
-        return {changeBoard, getBoard}
+    const resetBoard = () => {
+        _board = ["", "", "", "", "", "", "", "", ""]
+    }
+        return {changeBoard, getBoard, resetBoard}
      
 
 })();
 
-
-
-
-
-
-const displayController = (function () {
-    if (gameBoard.getBoard[index])
-
-
-})();
 
 const gameCount = (function () {
 
@@ -85,29 +65,47 @@ const clearDisplay = (function () {
 
 })(); 
 
-const controlPlay = (function () {
-
-
-
-
+const controlPlay = (() => {
+    const preventClick = (index) => {
+        document.querySelectorAll(".section");
+            section[index].className = "sectionfreeze";
+           
+        };
+    const activateClick = () => {
+        document.getElementById('zero').className = "section";
+        document.getElementById('one').className = "section";
+        document.getElementById('two').className = "section";
+        document.getElementById('three').className = "section";
+        document.getElementById('four').className = "section";
+        document.getElementById('five').className = "section";
+        document.getElementById('six').className = "section";
+        document.getElementById('seven').className = "section";
+        document.getElementById('eight').className = "section";
+    };
+    return {preventClick, activateClick}
 })();
 
 
 
-
-
-function newPlayer(name) {
-    this.name = name
-    const getPlayerName = () => {
-
-    };
-     return { getPlayerName, name}
+function newPlayer(name, token) {
+    const getName = () => name
+    const getToken = () => token
+     return { getName, getToken}
 };
-player1Name = ''
-player2Name = ''
-const player1 = newPlayer(player1Name);
-const player2 = newPlayer(player2Name);
+
+const playerOne = newPlayer(prompt("Enter Player One Name"), "X");
+const playerTwo = newPlayer(prompt("Enter Player Two Name"), "O");
 
 
+section.forEach((div) => {
+    let index = div.getAttribute("value")
+    div.addEventListener("click", () => {
+        console.log('Hello ' + `${index}`)   
+        gameCount.addOX()
+        gameBoard.changeBoard(index)
+        div.innerHTML = `${gameBoard.getBoard[index]}`
+        controlPlay.preventClick(index)
 
+    });
 
+}); 
